c73c1424a5ab92f3470f1452611d54155c3984f5
Melanie Leal <melanie.lealb09@gmail.com>
2023-02-24 17:21:39 -0600

    corregir codigo

--- Codigo6.java
+++ Codigo6.java
@@ -1,17 +1,17 @@
 public class codigo6 {
 	  
-	    int[] n = int[20];
+	    int[] n = new int[20];//se agregó el new
 
-	    for (int i = 0; i < 20; i+) {
+	    for (int i = 0; i < 20; i++) {//faltaba signo + en el for
 	      n[i] = (int)(Math.random() * 381) + 20;
-	      System.print(n[i] + " ");
+	      System.out.println(n[i] + " "); //completar el print
 	    }
 	    
-	    System.out.printl("\n¿Qué números quiere resaltar? ");
+	    System.out.println("\n¿Qué números quiere resaltar? ");//faltaba n en rl print
 	    System.out.print("(1 – los múltiplos de 5, 2 – los múltiplos de 7): ");
-	    int opcion = Integer.parseInt(System.console().readLine();
+	    int opcion = Integer.parseInt(System.console().readLine();//faltaba paréntesis
 
-	    int multiplo = (opcion == 1) : 5 ? 7;
+	    int multiplo = (opcion == 1) ? 5 : 7; //esta mal este ternario, cambio del lugar de ? y :
 
 	    foreach (char e : n) {
 	      if (e % multiplo == 0) {
